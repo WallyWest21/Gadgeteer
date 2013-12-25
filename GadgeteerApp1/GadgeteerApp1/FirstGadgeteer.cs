abb0025ba6fa12530d11a13951d738e772f7e7a9
WallyWest21 <candymanis@hotmail.com>
2013-12-25 00:39:32 -0500

    1st save

---
+++ GadgeteerApp1/GadgeteerApp1/FirstGadgeteer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+
+
+namespace GadgeteerApp1
+{
+    public partial class Program
+
+
+    {
+
+        
+
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+
+              // Subscribe to the event that will fire when an RFID tag is properly read
+   // rfid.CardIDReceived += new RFID.CardIDRecievedEventHandler(rfid_CardIDRecieved);
+            /*******************************************************************************************
+            Modules added in the Program.gadgeteer designer view are used by typing 
+            their name followed by a period, e.g.  button.  or  camera.
+            
+            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
+                button.ButtonPressed +=<tab><tab>
+            
+            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
+                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
+                timer.Tick +=<tab><tab>
+                timer.Start();
+            *******************************************************************************************/
+            display_N18.SimpleGraphics.DisplayTextInRectangle("you are not still a loser alain",
+                0, 0, 125, 200, GT.Color.Green, Resources.GetFont(Resources.FontResources.NinaB));
+
+            
+ 
+            
+// The event handler that is called when the event is fired.
+// The ID parameter contains the ID that was read from the card
+
+           
+            // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
+            Debug.Print("Program Started");
+        }
+
+        //void rfid_CardIDRecieved(RFID sender, string ID)
+        //{
+        //    // Display the ID to the screen
+        //    //Debug.Print(ID);
+
+        //    display_N18.SimpleGraphics.DisplayTextInRectangle("NICE",
+        //                0, 0, 150, 200, GT.Color.Green, Resources.GetFont(Resources.FontResources.NinaB));
+        //}
+    }
+
+  
+}
---
+++ GadgeteerApp1/GadgeteerApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+using LCD = Gadgeteer.Modules.GHIElectronics.Display_N18;
+
+
+namespace GadgeteerApp1
+{
+    public partial class Program
+    {
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+            /*******************************************************************************************
+            Modules added in the Program.gadgeteer designer view are used by typing 
+            their name followed by a period, e.g.  button.  or  camera.
+            
+            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
+                button.ButtonPressed +=<tab><tab>
+            
+            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
+                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
+                timer.Tick +=<tab><tab>
+                timer.Start();
+            *******************************************************************************************/
+
+            //display.Initialize(4000);
+            //mMainWindow = display.WPFWindow;
+            //mCanvas = new Canvas();
+            //mMainWindow.Child = mCanvas;
+
+            ////Add a gray background.
+            //mBackground = new Border();
+            //mBackground.Background = new SolidColorBrush(Colors.Gray);
+            //mBackground.Width = 128;
+            //mBackground.Height = 160;
+            //mCanvas.Children.Add(mBackground);
+            //Canvas.SetLeft(mBackground, 0);
+            //Canvas.SetTop(mBackground, 0);
+            //// Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
+            //Debug.Print("Program Started");
+
+            
+
+
+
+
+
+        }
+    }
+}
